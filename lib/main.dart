5c538bbe326d1b4369c22cfb9d90bc8dcd4d3edb
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-06 20:45:39 +0530

    Responsive List Grid

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,50 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Responsive List/Grid'),
         ),
         body: const Center(
-          child: Text(
-            'Welcome to Flutter Responsive List/Grid!',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 24),
-          ),
+          child: ResponsiveListGrid(),
         ),
       ),
+    );
+  }
+}
+
+class ResponsiveListGrid extends StatelessWidget {
+  const ResponsiveListGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.blue[100 * (index % 9 + 1)],
+                child: Center(
+                  child: Text('Grid Item $index'),
+                ),
+              );
+            },
+          );
+        } else {
+          return ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                color: Colors.green[100 * (index % 9 + 1)],
+                child: ListTile(title: Text('List Item $index')),
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
