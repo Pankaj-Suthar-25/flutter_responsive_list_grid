d072614099a4bc34067fc6e7b27488920cc9f288
Pankaj-Suthar-25 <ps25softwaredeveloper@gmail.com>
2025-06-08 15:41:05 +0530

    Provider Responsive List Grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Responsive List/Grid'),
         ),
         body: const Center(
-          child: ResponsiveListGrid(),
+          child: ProviderResponsiveListGrid(),
         ),
       ),
     );
@@ -55,7 +56,8 @@ class ResponsiveListGrid extends StatelessWidget {
             itemCount: 10,
             itemBuilder: (context, index) {
               return Card(
-                margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
                 color: Colors.green[100 * (index % 9 + 1)],
                 child: ListTile(title: Text('List Item $index')),
               );
@@ -63,6 +65,121 @@ class ResponsiveListGrid extends StatelessWidget {
           );
         }
       },
+    );
+  }
+}
+
+class ItemProvider with ChangeNotifier {
+  final List<String> _allItems = List.generate(20, (index) => 'Item $index');
+  List<String> _items = List.generate(10, (index) => 'Item $index');
+
+  List<String> get items => _items;
+
+  void refreshItems() {
+    _items = List.generate(10, (index) => 'Item ${index + 10}');
+    notifyListeners();
+  }
+
+  void filterItems(String query) {
+    _items = _allItems
+        .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    notifyListeners();
+  }
+}
+
+class ProviderResponsiveListGrid extends StatelessWidget {
+  const ProviderResponsiveListGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ItemProvider(),
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Column(
+          children: [
+            _buildSearchBar(context),
+            Expanded(
+              child: Consumer<ItemProvider>(
+                builder: (context, provider, child) {
+                  return RefreshIndicator(
+                    onRefresh: () async {
+                      provider.refreshItems();
+                    },
+                    child: constraints.maxWidth > 600
+                        ? _buildGridView(provider)
+                        : _buildListView(provider),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      }),
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          prefixIcon: const Icon(Icons.search),
+          hintText: 'Search...',
+        ),
+        onChanged: (value) {
+          Provider.of<ItemProvider>(context, listen: false).filterItems(value);
+        },
+      ),
+    );
+  }
+
+  Widget _buildGridView(ItemProvider provider) {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: provider.items.length,
+        itemBuilder: (context, index) {
+          return _buildItemCard(provider.items[index], index);
+        });
+  }
+
+  Widget _buildListView(ItemProvider provider) {
+    return ListView.builder(
+      itemCount: provider.items.length,
+      itemBuilder: (context, index) {
+        return _buildItemTile(provider.items[index], index);
+      },
+    );
+  }
+
+  Widget _buildItemCard(String item, int index) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      color: Colors.blue[100 * (index % 9 + 1)],
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(item, style: const TextStyle(fontSize: 18)),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItemTile(String item, int index) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      color: Colors.green[100 * (index % 9 + 1)],
+      child: ListTile(
+        title: Text(item),
+      ),
     );
   }
 }
